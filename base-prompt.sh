c7aa0bdf818a91c4555e544a73578687a9f91adf
Jordon Storfa <looking.promising@gmail.com>
2014-05-06 12:56:39 -0600

    fixed issue with incorrectly escaped color codes

--- base-prompt.sh
+++ base-prompt.sh
@@ -2,7 +2,7 @@
 #  A base prompt which can be sourced from any custom prompts
 # #######################################################################################
 
-# #######################################################################################
+#########################################################################################
 #  import the zshuery zsh shell
 #  git clone git://github.com/myfreeweb/zshuery.git ~/.zsh/zshuery
 # #######################################################################################
@@ -13,6 +13,12 @@ load_lol_aliases
 load_completion ~/.zsh/zshuery/completion
 load_correction
 
+# reconfigure the colors for OSX and use colorized ls command
+if is_mac; then
+    export LSCOLORS=cxfxcxdxbxcxcxabagacad
+    alias ls='ls -G' # use colored output
+fi
+
 # #######################################################################################
 # import the very nice zsh-git-prompt
 # git clone git://github.com/olivierverdier/zsh-git-prompt.git ~/.zsh/git-prompt
@@ -20,21 +26,6 @@ load_correction
 source ~/.zsh/git-prompt/zshrc.sh
 
 # #######################################################################################
-#  define some custom colors
-# #######################################################################################
-        RED=$'\033[0;31m'
-     YELLOW=$'\033[0;33m'
-      GREEN=$'\033[0;32m'
-       BLUE=$'\033[0;34m'
-     PURPLE=$'\033[1;35m'
-  LIGHT_RED=$'\033[1;31m'
-LIGHT_GREEN=$'\033[1;32m'
-      WHITE=$'\033[1;37m'
- LIGHT_GRAY=$'\033[0;37m'
-  
-PROMPT_SPACE=''
-
-# #######################################################################################
 # set the default prompts
 # #######################################################################################
-prompts '${GREEN}$(COLLAPSED_DIR)%{$reset_color%} $(virtualenv_info)$(git_super_status)%{$fg[yellow]%}$(prompt_char)%{$reset_color%} ' '%{$fg[red]%}$(ruby_version)%{$reset_color%}'
+prompts '%{$fg[green]%}$(COLLAPSED_DIR)%{$reset_color%} $(git_super_status)%{$fg[yellow]%}$(prompt_char $)%{$reset_color%} ' '%{$fg[red]%}$(ruby_version)%{$reset_color%}'
--- multi-line-prompt.sh
+++ multi-line-prompt.sh
@@ -12,6 +12,6 @@
 source ~/.zsh/base-prompt.sh
 
 # set the prompts  -- NOTE: the newline is in quotes and on multiple lines
-prompts '${LIGHT_RED}%n${BLUE}@${LIGHT_RED}%m ${PURPLE}{%D{%A %b %d} ${PURPLE}%@}
+prompts '%{$fg[red]%}%n%{$fg[white]%}@%{$fg[red]%}%m %{$fg[cyan]%}{%D{%A %b %d} %{$fg[cyan]%}%@}
 %{$reset_color%}[%~]
-$(virtualenv_info)$(git_super_status)%{$fg[yellow]%}$(prompt_char)%{$reset_color%} ' '%{$fg[red]%}$(ruby_version)%{$reset_color%}'
+$(git_super_status)%{$fg[yellow]%}$(prompt_char)%{$reset_color%} ' '%{$fg[red]%}$(ruby_version)%{$reset_color%}'
